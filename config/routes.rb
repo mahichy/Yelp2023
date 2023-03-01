09afc7e81c17567ed40041c98f055791dcb8e392
mahichy <mohiuddinoshim@yahoo.com>
2023-03-01 19:27:55 +0600

    set the home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :restaurants
   get 'pages/about'
   get 'pages/contact'
+
+  root "restaurants#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
